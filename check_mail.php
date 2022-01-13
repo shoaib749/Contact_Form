f59763620ca6a97aff4b0210216f6f936884d8c7
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-01-13 19:45:09 +0530

    First Commit

---
+++ check_mail.php
@@ -0,0 +1,10 @@
+<?php
+//the subject
+$sub = "test";
+//the message
+$msg = "this is test ";
+//recipient email here
+$rec = "shoaibshamimansari@gmail.com";
+//send email
+mail($rec,$sub,$msg);
+?>
